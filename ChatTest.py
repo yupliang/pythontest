501518ebd852982f33efca02d177e22345a44d46
yupeiliang <yupeiliang@fotoable.com>
2020-10-28 17:57:19 +0800

    chat

--- ChatTest.py
+++ ChatTest.py
@@ -1,33 +1,30 @@
 from chatterbot import ChatBot
-
-
-# Uncomment the following lines to enable verbose logging
-import logging
-logging.basicConfig(level=logging.INFO)
-
-# Create a new instance of a ChatBot
-bot = ChatBot(
-    'Terminal',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
-        'chatterbot.logic.BestMatch'
-    ],
-    database_uri='sqlite:///database.db'
-)
-
-print('Type something to begin...')
-
-# The following loop will execute each time the user enters input
-while True:
-    try:
-        user_input = input()
-
-        bot_response = bot.get_response(user_input)
-
-        print(bot_response)
-
-    # Press ctrl-c or ctrl-d on the keyboard to exit
-    except (KeyboardInterrupt, EOFError, SystemExit):
-        break
+from chatterbot.trainers import ChatterBotCorpusTrainer
+chatbot = ChatBot("ChineseChatbot1")
+trainer=ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.chinese")
+question = '早上好'
+response = chatbot.get_response(question)
+print(question)
+print(response)
+print("###########################################")
+question = "很高兴认识你"
+print(question)
+print(chatbot.get_response(question))
+print("###########################################")
+question = "嗨，最近如何?"
+print(question)
+print(chatbot.get_response(question))
+print("###########################################")
+question = "复杂优于晦涩"
+print(question)
+print(chatbot.get_response(question))
+print("###########################################")
+question = "面对模棱两可，拒绝猜测的诱惑."
+print(question)
+print(chatbot.get_response(question))
+print("###########################################")
+question = "生命、宇宙以及世间万物的终极答案是什么?"
+print(question)
+print(chatbot.get_response(question))
+print("###########################################")
